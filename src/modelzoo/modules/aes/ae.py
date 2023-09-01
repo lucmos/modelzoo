04aafe2be4b571ac2325e329d3582f0217f2d282
Luca Moschella <luca.moschella94@gmail.com>
2023-09-01 08:47:40 +0000

    Remove unused function

--- src/modelzoo/modules/aes/ae.py
+++ src/modelzoo/modules/aes/ae.py
@@ -112,12 +112,3 @@ class VanillaAE(nn.Module):
 
         samples = self.decode(z)
         return samples
-
-    def generate(self, x: Tensor, **kwargs) -> Tensor:
-        """
-        Given an input image x, returns the reconstructed image
-        :param x: (Tensor) [B x C x H x W]
-        :return: (Tensor) [B x C x H x W]
-        """
-
-        return self.forward(x)[0]
