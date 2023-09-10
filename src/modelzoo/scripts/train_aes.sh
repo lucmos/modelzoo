648e20e3485690233409548d50be6e554eeaff7f
Luca Moschella <luca.moschella94@gmail.com>
2023-09-10 19:27:53 +0200

    Setup batch training script

--- src/modelzoo/scripts/train_aes.sh
+++ src/modelzoo/scripts/train_aes.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 
-for model in ae
+for model in ae vae linearized_ae linearized_vae
 do
        for dataset in mnist fmnist cifar10 cifar100
        do
               for seed in 0 1 2 3 4
               do
-                     for decoder_norm in none
-                     do
-                            python src/modelzoo/run.py nn=aes train=reconstruction \
-                                   nn/module/model=$model \
-                                   nn/data/datasets/vision/hf@nn.data.datasets.anchors=$dataset \
-                                   nn/data/datasets/vision/hf@nn.data.datasets.hf=$dataset \
-                                   nn/module/model/decoder_in_normalization=$decoder_norm \
-                                   train.seed_index=$seed \
-                                   core.tags="[aes,$model,$dataset,$decoder_norm,run$seed]"
-                            echo
-                     done
+                     HYDRA_FULL_ERROR=1 python src/modelzoo/run.py nn=aes train=reconstruction \
+                            nn/module/model=$model \
+                            nn/data/datasets/vision/hf@nn.data.datasets.anchors=$dataset \
+                            nn/data/datasets/vision/hf@nn.data.datasets.hf=$dataset \
+                            train.seed_index=$seed \
+                            core.tags="[aes,$model,$dataset,run$seed]"
+                     echo
               done
        done
 done
